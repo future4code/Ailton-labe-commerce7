249c076ba463d7367186166cc5d2ed549a039f65
Larissa Utermohl <larissautermohl@gmail.com>
2022-06-15 19:10:53 -0300

    larissa

--- src/App.js
+++ src/App.js
@@ -1,26 +1,69 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import styled from 'styled-components';
+import Filtros from "./components/Filtro.js";
+import Produtos from "./components/Produtos.js";
+import Carrinho from "./components/Carrinho.js";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+
+class App extends React.Component{
+  state = {
+    produtos: "produtos",
+    ordenacao: "crescente",
+    valorMaximo: Infinity,
+    valorMinimo: 0,
+    buscarProduto: "",
+    };
+
+    alteraInputOrdenacao = (event) => {
+      this.setState({ ordenacao: event.target.value });
+    };
+  
+    onChangeInputValorMinimo = (event) => {
+      this.setState({ valorMinimo: Number(event.target.value) });
+    };
+  
+    onChangeInputValorMaximo = (event) => {
+      this.setState({ valorMaximo: Number(event.target.value) });
+    };
+  
+    onChangeInputBusca = (event) => {
+      this.setState({ buscarProduto: event.target.value });
+    };
+
+    render(){
+      return(
+      
+        <div className="container">
+          <header>header</header>
+          {/* <section className= "main-container"> */}
+        {/* <ContainerProdutos>
+        <Produtos>
+
+        </Produtos>
+
+        </ContainerProdutos> */}
+
+        {/* <ContainerCarrinho>
+          <h3>Carrinho</h3>
+          <div>Carrinhos</div>
+          <p>Valor Totatl: </p>
+        </ContainerCarrinho>
+          
+          </section>
+          <footer>
+            <p class="text"> 
+            <b> No Copyright </b> </p>
+          <TextoRodape>Larissa, Fabiano, Giovanna 
+            </TextoRodape>
+            </footer> */}
+          </div>
+
+         
+      )
+    }
+  
+  }
+  
+    
 
 export default App;
---
+++ src/components/Filtro.js
@@ -0,0 +1,37 @@
+import React, { Component } from 'react';
+import styled from 'styled-components';
+
+
+const Titulo =styled.h3`
+margin-bottom = 10px;
+`
+
+ class Filtro extends Component {
+  
+  render() {
+
+    return (
+
+      <div>
+
+<Titulo>Filtros</Titulo>
+
+<label>Valor Minimo:
+  <input value={this.props.InputValorMinimo} onChange={this.props.onChangeInputValorMinimo}/>
+</label>
+
+<label>Valor Maximo:
+<input value={this.props.InputValorMaximo} onChange={this.props.onChangeInputValorMaximo}/>
+
+</label>
+
+<label> 
+  Buscar:
+  <input></input>
+</label>
+      </div>
+    )
+  }
+}
+
+export default Filtro;
